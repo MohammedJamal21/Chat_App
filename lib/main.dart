f750e8c2a757af2f658bc110782749d5d89c2885
MohammedJamal21 <mojamal.it@gmail.com>
2023-02-14 19:00:18 +0100

    SplashScreen added

--- lib/main.dart
+++ lib/main.dart
@@ -3,27 +3,36 @@ import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 import 'screens/auth_screens/signup_screen.dart';
+import 'screens/splash_screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const ChatApp());
+  runApp(ChatApp());
 }
 
 class ChatApp extends StatelessWidget {
-  const ChatApp({super.key});
+  ChatApp({super.key});
 
-  // This widget is the root of your application.
+  final Future<FirebaseApp> _initializeApp = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Chat App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SignupScreen(),
-    );
+    return FutureBuilder(
+        future: _initializeApp,
+        builder: (context, app) {
+          if (app.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          return MaterialApp(
+            title: 'Chat App',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            home: const SignupScreen(),
+          );
+        });
   }
 }

f2fc28c1b6e295bc89929eb7e05fed3119e32448
MohammedJamal21 <mojamal.it@gmail.com>
2023-02-18 15:43:20 +0100

    Added ListTiles and a Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/user_screens.dart/settings_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -5,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'screens/auth_screens/signup_screen.dart';
 import 'screens/splash_screens/splash_screen.dart';
-import 'screens/user_screens.dart/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,7 +40,7 @@ class ChatApp extends StatelessWidget {
                       if (!snapshot.hasData) {
                         return const SignupScreen();
                       }
-                      return const HomeScreen();
+                      return const SettingsScreen();
                     },
                   ),
           );
--- lib/screens/user_screens.dart/settings_screen.dart
+++ lib/screens/user_screens.dart/settings_screen.dart
@@ -10,6 +10,55 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Settings',
+        ),
+      ),
+      body: Column(
+        children: [
+          ListTile(
+            title: const Text('Edit Account'),
+            tileColor: Colors.blue,
+            trailing: IconButton(
+                onPressed: () {}, icon: const Icon(Icons.arrow_forward)),
+          ),
+          ListTile(
+            title: const Text('Change Password'),
+            tileColor: Colors.blue,
+            trailing: IconButton(
+                onPressed: () {}, icon: const Icon(Icons.arrow_forward)),
+          ),
+          ListTile(
+            title: const Text('Security & Privacy'),
+            tileColor: Colors.blue,
+            trailing: IconButton(
+                onPressed: () {}, icon: const Icon(Icons.arrow_forward)),
+          ),
+          ListTile(
+            title: const Text('Language'),
+            tileColor: Colors.blue,
+            trailing: IconButton(
+                onPressed: () {}, icon: const Icon(Icons.arrow_forward)),
+          ),
+          SwitchListTile(
+            title: const Text('App Notification'),
+            tileColor: Colors.blue,
+            value: false,
+            onChanged: (value) {},
+          ),
+          SwitchListTile(
+            title: const Text('Dark Theme'),
+            tileColor: Colors.blue,
+            value: false,
+            onChanged: (value) {},
+          ),
+          const Spacer(),
+          ElevatedButton(onPressed: () {}, child: const Text('Log Out'))
+        ],
+      ),
+    );
   }
 }
